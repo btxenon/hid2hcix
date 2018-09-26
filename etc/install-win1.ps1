7404c875eac14d8aa03a4b79ab86c01e5afc21d5
btxenon <bt_xenon@hotmail.com>
2018-09-27 01:35:51 +0200

    Update install-win1.ps1

--- etc/install-win1.ps1
+++ etc/install-win1.ps1
@@ -4,4 +4,4 @@ $dest = $SystemProperty.'Common Startup' + "\hid2hcix.exe"
 # $UserProperty = $(Get-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders')
 # $dest = $UserProperty.Startup
 echo $dest
-Invoke-WebRequest -Uri "https://github.com/cho45/hid2hcix/releases/download/v1.0.0/hid2hcix.exe" -OutFile $dest
+Invoke-WebRequest -Uri "http://github.com/cho45/hid2hcix/releases/download/v1.0.0/hid2hcix.exe" -OutFile $dest
