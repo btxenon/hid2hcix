96990f0ae0582bba830032c17f9685687e2c48d7
btxenon <bt_xenon@hotmail.com>
2018-09-27 01:34:58 +0200

    Update install-win.bat

--- install-win.bat
+++ install-win.bat
@@ -1,5 +1,5 @@
 @echo off
-set command=Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cho45/hid2hcix/master/etc/install-win.ps1" -OutFile "%TEMP%\install-win.ps1"
+set command=Invoke-WebRequest -Uri "http://raw.githubusercontent.com/btxenon/hid2hcix/master/etc/install-win.ps1" -OutFile "%TEMP%\install-win.ps1"
 set command=%command:"=\"%
 echo %command%
 powershell -NoProfile -ExecutionPolicy Unrestricted -Command "%command%"
